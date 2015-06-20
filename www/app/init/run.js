561af29ddd686b3aab62a49e863bc025528eee30
Sbats <simon.bats12@gmail.com>
2015-06-20 12:49:16 +0200

    add missing ;

--- www/app/init/run.js
+++ www/app/init/run.js
@@ -14,7 +14,7 @@ function IonicInit($ionicPlatform, InitService) {
         if (!String.prototype.capitalizeFirstLetter) {
             String.prototype.capitalizeFirstLetter = function() {
                 return this.charAt(0).toUpperCase() + this.slice(1);
-            }
+            };
         }
 
         InitService.initApp();
